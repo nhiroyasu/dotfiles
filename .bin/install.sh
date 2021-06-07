0b323968547cb7ba4976f71e98d190cfaf215958
hiroyasu <namid224y@gmail.com>
2021-06-07 19:14:10 +0900

    update

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 set -ue
 
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+dotdir=$(dirname ${script_dir})
 # dotfileをシンボリックリンクでホームディレクトリに配置
 link_to_homedir() {
   command echo "backup old dotfiles..."
-  if [ ! -d "$HOME/.dotbackup" ];then
+  if [ ! -d "$HOME/.dotbackup" ]; then
     command echo "$HOME/.dotbackup not found. Auto Make it"
     command mkdir "$HOME/.dotbackup"
   fi
 
-  local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-  local dotdir=$(dirname ${script_dir})
-  if [[ "$HOME" != "$dotdir" ]];then
+  if [[ "$HOME" != "$dotdir" ]]; then
     for f in $dotdir/.??*; do
       [[ `basename $f` == ".git" ]] && continue
-      if [[ -L "$HOME/`basename $f`" ]];then
+      if [[ -L "$HOME/`basename $f`" ]]; then
         command rm -f "$HOME/`basename $f`"
       fi
-      if [[ -e "$HOME/`basename $f`" ]];then
+      if [[ -e "$HOME/`basename $f`" ]]; then
         command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
       fi
       command ln -snf $f $HOME
@@ -42,8 +42,8 @@ change_shell_to_zsh() {
 # powerlevel10kをインストール
 install_powerlevel10k() {
   command echo "setup powerlevel10k"
-  if [[ -d "$HOME/powerlevel10k" ]];then
-    command echo "powerlevel10k is alread exist"
+  if [[ -d "$HOME/powerlevel10k" ]]; then
+    command echo "powerlevel10k is already exist"
   else
     command echo "install powerlevel10k"
     command git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
@@ -51,10 +51,34 @@ install_powerlevel10k() {
   fi
 }
 
+# ハイライトのツールをインストール
+install_syntax_highlight() {
+    if [[ -d "$HOME/.zsh/zsh-syntax-highlighting" ]]; then
+        command echo "zsh-syntax-highlighting is already exist"
+    else
+        command echo "install setup_zsh_syntax_highlight"
+        command git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/zsh-syntax-highlighting
+        echo "source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
+    fi
+}
+
+# オートサジェスチョンをインストール
+install_auto_suggestion() {
+    if [[ -d "$HOME/.zsh/zsh-autosuggestions" ]]; then
+        command echo "zsh-autosuggestion is already exist"
+    else
+        command echo "install setup_zsh_autosuggestion"
+        command git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+        echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $HOME/.zshrc
+    fi
+}
+
 link_to_homedir
 config_git
 change_shell_to_zsh
 install_powerlevel10k
+install_syntax_highlight
+install_auto_suggestion
 
 # zshを再起動
 command zsh
